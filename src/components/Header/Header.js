c92dfe0661bf0da358999f6f42b3b56423edf7c1
checkiamsiam <issiam02415@gmail.com>
2022-04-05 09:35:21 +0600

    hamberger making

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg'
 import { Link} from 'react-router-dom';
+import { AiOutlineMenu } from 'react-icons/ai';
+import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
 const Header = () => {
+  const [open , setOpen] = useState(true);
   return (
     <div className='header'>
       <div className="title"> <Link to='/'><img  src={logo} alt="" /></Link></div>
       <div className="nav">
-        <nav>
+        <div onClick={()=> setOpen(!open)} className="ham-berger">
+          {open ? <AiOutlineMenu/> :  <MdOutlineRestaurantMenu/>}
+       
+        </div>
+        <nav className={`${open ? 'none' : 'block'}`}>
           <Link className='nav-link' to="/">Home</Link>
           <Link className='nav-link' to="/shop">Shop</Link>
           <Link className='nav-link' to="/order">Order</Link>
